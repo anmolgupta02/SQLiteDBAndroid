a687043f47e0dba8e8074c324457077359cc378a
anmol <anmolgupta02@gmail.com>
2020-05-11 05:21:33 +0530

    Implemented update and Delete methods

--- app/src/main/java/com/example/sqlitedb/MainActivity.java
+++ app/src/main/java/com/example/sqlitedb/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -86,17 +87,52 @@ public class MainActivity extends AppCompatActivity {
 
                 if(cursor.getCount() == 0){
                     showMessage("Error", "No data in db currently");
+                }else{
+
+                    while(cursor.moveToNext()){
+                        buffer.append("ID: "+cursor.getString(0)+"\n");
+                        buffer.append("Name: "+cursor.getString(1)+"\n");
+                        buffer.append("Email: "+cursor.getString(2)+"\n");
+                        buffer.append("Course Count: "+cursor.getString(3)+"\n\n");
+                    }
+
+                    showMessage("Results", buffer.toString());
                 }
 
-                while(cursor.moveToNext()){
-                    buffer.append("ID: "+cursor.getString(0)+"\n");
-                    buffer.append("Name: "+cursor.getString(1)+"\n");
-                    buffer.append("Email: "+cursor.getString(2)+"\n");
-                    buffer.append("Course Count: "+cursor.getString(3)+"\n\n");
-                }
-
-                showMessage("Results", buffer.toString());
             }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isUpdated = myDB.updateData(etId.getText().toString(),
+                        etName.getText().toString(),
+                        etEmail.getText().toString(),
+                        etCourseCount.getText().toString());
+
+                if(isUpdated){
+                    Toast.makeText(MainActivity.this, "Updated successfully", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Oops", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+
+        });
+
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  Integer deletedRow = myDB.deleteData(etId.getText().toString());
+                  if(deletedRow>0){
+                      Toast.makeText(MainActivity.this, "Deleted Sucessfullly", Toast.LENGTH_SHORT).show();
+                  }
+
+
+            }
+
         });
     }
 
