429c86967fc555246333b48d69ca3f2b617309a0
anmol <anmolgupta02@gmail.com>
2020-05-11 03:00:09 +0530

    Helper Method in Main. Variable Dec Done.
    Signed-off-by: anmol <anmolgupta02@gmail.com>

--- app/src/main/java/com/example/sqlitedb/MainActivity.java
+++ app/src/main/java/com/example/sqlitedb/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.sqlitedb;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etId, etName, etEmail, etCourseCount;
+    Databasehelper myDB;
+    Button btnAdd, btnGetdata, btnUpdate, btnDelete, btnViewAll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDB = new Databasehelper(this);
+
+        btnAdd = findViewById(R.id.button_add);
+        btnDelete = findViewById(R.id.button_delete);
+        btnGetdata = findViewById(R.id.button_view);
+        btnViewAll = findViewById(R.id.button_viewAll);
+        btnUpdate = findViewById(R.id.button_update);
+
+        etId = findViewById(R.id.editText_id);
+        etName = findViewById(R.id.editText_name);
+        etEmail = findViewById(R.id.editText_email);
+        etCourseCount = findViewById(R.id.editText_CC);
+
+        showMessage("test", "testing done");
+
+    }
+
+    public void showMessage(String Title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.create();
+        builder.setCancelable(true);
+        builder.setTitle(Title);
+        builder.setMessage(Message);
+        builder.show();
+
     }
 }
